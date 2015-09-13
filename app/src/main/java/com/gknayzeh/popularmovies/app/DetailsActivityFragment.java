43e277a8bf2e87b578a5441dd09ea735ea283c37
Ghassan Knayzeh <g.knayzeh@gmail.com>
2015-09-13 19:59:30 -0400

    Use the loader

--- app/src/main/java/com/gknayzeh/popularmovies/app/DetailsActivityFragment.java
+++ app/src/main/java/com/gknayzeh/popularmovies/app/DetailsActivityFragment.java
@@ -1,8 +1,12 @@
 package com.gknayzeh.popularmovies.app;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
@@ -14,17 +18,50 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.gknayzeh.popularmovies.app.data.MovieContract;
+
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class DetailsActivityFragment extends Fragment {
+public class DetailsActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private final static String LOG_TAG = DetailsActivityFragment.class.getSimpleName();
 
     private final static String POPULAR_MOVIES_HASH_TAG = " #PopularMovies";
 
+    private ShareActionProvider mShareActionProvider;
     private String mMovieString;
+
+    private static final int MOVIE_DETAILS_LOADER = 0;
+
+    private static final String[] MOVIE_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            MovieContract.MovieEntry._ID,
+            MovieContract.MovieEntry.COL_POSTER_PATH,
+            MovieContract.MovieEntry.COL_BACKDROP_PATH,
+            MovieContract.MovieEntry.COL_MOVIE_TITLE,
+            MovieContract.MovieEntry.COL_OVERVIEW,
+            MovieContract.MovieEntry.COL_RELEASE_DATE,
+            MovieContract.MovieEntry.COL_VOTE_AVERAGE,
+            MovieContract.MovieEntry.COL_SORT_BY,
+            MovieContract.MovieEntry.COL_SORT_ORDER
+    };
+
+    static final int COL_MOVIE_ID = 0;
+    static final int COL_POSTER_PATH = 1;
+    static final int COL_BACKDROP_PATH = 2;
+    static final int COL_MOVIE_TITLE = 3;
+    static final int COL_OVERVIEW = 4;
+    static final int COL_RELEASE_DATE = 5;
+    static final int COL_VOTE_AVERAGE = 6;
+    static final int COL_SORT_BY = 7;
+    static final int COL_SORT_ORDER = 8;
 
     public DetailsActivityFragment() {
         setHasOptionsMenu(true);
@@ -34,13 +71,15 @@ public class DetailsActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_details, container, false);
-        Intent intent = getActivity().getIntent();
-        if (intent != null) {
-            mMovieString = intent.getDataString();
-            ((TextView) rootView.findViewById(R.id.detail_text)).setText(mMovieString);
-        }
-        return rootView;
+        return inflater.inflate(R.layout.fragment_details, container, false);
+
+//        View rootView = inflater.inflate(R.layout.fragment_details, container, false);
+//        Intent intent = getActivity().getIntent();
+//        if (intent != null) {
+//            mMovieString = intent.getDataString();
+//            ((TextView) rootView.findViewById(R.id.detail_text)).setText(mMovieString);
+//        }
+//        return rootView;
     }
 
     @Override
@@ -49,7 +88,7 @@ public class DetailsActivityFragment extends Fragment {
 
         MenuItem menuItem = menu.findItem(R.id.action_share);
 
-        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
 
         if (mShareActionProvider != null) {
             mShareActionProvider.setShareIntent(createShareMovieDetailsIntent());
@@ -62,8 +101,59 @@ public class DetailsActivityFragment extends Fragment {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT,
-                mMovieString + POPULAR_MOVIES_HASH_TAG);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mMovieString + POPULAR_MOVIES_HASH_TAG);
         return shareIntent;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(MOVIE_DETAILS_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.v(LOG_TAG, "In onCreateLoader");
+        Intent intent = getActivity().getIntent();
+        if (intent == null) {
+            return null;
+        }
+
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        return new CursorLoader(
+                getActivity(),
+                intent.getData(),
+                MOVIE_COLUMNS,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.v(LOG_TAG, "In onLoadFinished");
+        if (!data.moveToFirst()) { return; }
+
+        String title = data.getString(COL_MOVIE_TITLE);
+        String overview = data.getString(COL_OVERVIEW);
+        String releaseDate = data.getString(COL_RELEASE_DATE);
+        String voteAverage = Double.toString(data.getDouble(COL_VOTE_AVERAGE));
+        String posterPath = data.getString(COL_POSTER_PATH);
+        String backdropPath = data.getString(COL_BACKDROP_PATH);
+
+        mMovieString = String.format("%s - %s - %s - %s - %s - %s", title, overview, releaseDate, voteAverage, posterPath, backdropPath);
+
+        TextView detailTextView = (TextView)getView().findViewById(R.id.detail_text);
+        detailTextView.setText(mMovieString);
+
+        // If onCreateOptionsMenu has already happened, we need to update the share intent now.
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareMovieDetailsIntent());
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) { }
 }
